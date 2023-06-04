0cf4fbb8036916daa076d2c60dc8cb5d60b9898f
Sayan Batabyal <sayanbatabyal.mav@gmail.com>
2023-06-04 20:19:18 +0530

    ui bugs resolved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
   return (
     <div className='flex items-center justify-between py-7 px-20 bg-slate-200'>
       <h1 className='text-3xl text-blue-700 font-bold  '>Welcome <span className='text-sm'>{userName}</span></h1>
-      <div className='w-1/4 flex justify-between'>
+      <div className='w-1/4 gap-2 flex justify-between'>
        <Link to={"/add"} className='font-bold text-blue-700'>+ New Contact</Link>
       <button className='font-bold text-blue-700 ' onClick={handleLogOut}>Log Out</button>
       </div>
