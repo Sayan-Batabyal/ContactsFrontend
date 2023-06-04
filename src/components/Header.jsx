7bd4e9ecb6ed5e2466878705151597dbb02f5012
Sayan Batabyal <sayanbatabyal.mav@gmail.com>
2023-06-04 20:32:50 +0530

    ui bugs resolved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         window.location.reload()
     }
   return (
-    <div className='flex items-center justify-between py-7 px-20 bg-slate-200'>
+    <div className='flex items-center justify-between py-7 px-2 lg:px-20 bg-slate-200'>
       <h1 className='text-3xl text-blue-700 font-bold  '>Welcome <span className='text-sm'>{userName}</span></h1>
       <div className='w-1/4 gap-2 flex justify-between'>
        <Link to={"/add"} className='font-bold text-blue-700'>+ New Contact</Link>
